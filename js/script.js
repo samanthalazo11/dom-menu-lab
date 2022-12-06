753a06dab67af939a08fed2d6e9dcdc5f623b25c
Samantha Lazo <lazoc.samantha@gmail.com>
2022-12-05 22:55:38 -0500

    feat: task 2.0 complete

--- js/script.js
+++ js/script.js
@@ -15,3 +15,8 @@ mainEl.innerHTML= '<h1>SEIR Rocks!</h1>';
 //Task.1.3
 
 mainEl.classList.add('flex-ctr');
+
+// Task 2.0
+
+const topMenuEl = document.getElementById('top-menu');
+console.log(topMenuEl);
